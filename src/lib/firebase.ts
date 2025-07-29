b2924cc7d854b277cf4943cbcc368586c8688d52
AMAN ARORA <f20221156@pilani.bits-pilani.ac.in>
2025-07-29 16:04:40 +0000

    already added

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,8 +11,8 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase for SSR
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { app, auth };
